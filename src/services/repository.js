edbc315a9cc97e87b089eb36784066aea3b74ee8
Irae Veras <irae.online@gmail.com>
2024-05-30 23:49:21 -0300

    remoção de variavel não utilizada

--- src/services/repository.js
+++ src/services/repository.js
@@ -1,4 +1,4 @@
-import { baseUrl, repositoryQtd, inputsNumbersRepository } from "../scripts/variables/variables.js";
+import { baseUrl, inputsNumbersRepository } from "../scripts/variables/variables.js";
 
 async function getRepositoriesGithub(userName) {
     const response = await fetch(`${baseUrl}/${userName}/repos?per_page=${inputsNumbersRepository.value}`);
